6df36d1e0c02e82f5b1db41d517563df3c886c84
ruper <dcasenillo@gmail.com>
2026-01-14 18:00:05 +0800

    some stuff
    di pa tapos

---
+++ Coffee.Kiosk.OrderingSystem/CoffeeKioskMainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Coffee.Kiosk.OrderingSystem
+{
+    partial class CoffeeKioskMainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            mainPanel = new Panel();
+            SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            mainPanel.BackColor = Color.White;
+            mainPanel.Dock = DockStyle.Fill;
+            mainPanel.Location = new Point(3, 24);
+            mainPanel.Name = "mainPanel";
+            mainPanel.Size = new Size(767, 733);
+            mainPanel.TabIndex = 0;
+            // 
+            // CoffeeKiosk
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(203, 183, 153);
+            ClientSize = new Size(773, 760);
+            Controls.Add(mainPanel);
+            FormStyle = FormStyles.ActionBar_None;
+            Name = "CoffeeKiosk";
+            Padding = new Padding(3, 24, 3, 3);
+            RightToLeft = RightToLeft.No;
+            Text = "KIOSK";
+            Load += CoffeeKiosk_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel mainPanel;
+    }
+}
--- Coffee.Kiosk.OrderingSystem/Program.cs
+++ Coffee.Kiosk.OrderingSystem/Program.cs
@@ -11,7 +11,7 @@ namespace Coffee.Kiosk.OrderingSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new CoffeeKioskMainForm());
         }
     }
 }
