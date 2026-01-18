00b6e57d1186d473feeb6288d49d8c6e2de3ed3a
Mult0 <taezarex@gmail.com>
2026-01-18 16:11:01 +0800

    AddingEmployeesToAccounts.
    CHANGES:
        -Created the MVC structure the CMS will follow
        -Dependency Injections for good practice
      specifics:
        -Adding employee function
        -Errors
    
    NOTES:
        -Havent fully tested it yet, the validation works tho
        -adding employees works too
        -I'll do other functions later
        -Apologies in advance about my poor excuse of a UI
        -Also, with the structure, its easy to change what attributes the employee has, the current state of the accounts table is just for testing.

---
+++ Coffee.Kiosk.CMS/Controllers/RegistrationController.cs
@@ -0,0 +1,42 @@
+﻿using Coffee.Kiosk.CMS.DTOs;
+using Coffee.Kiosk.CMS.Models;
+using Coffee.Kiosk.CMS.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee.Kiosk.CMS.Controllers
+{
+    public class RegistrationController
+    {
+
+        private readonly RegistrationValidation _validation;
+        private readonly RegistrationService _service;
+
+        public RegistrationController(RegistrationValidation validation, RegistrationService service)
+        {
+            _validation = validation ?? throw new ArgumentNullException(nameof(validation));    
+
+            _service = service ??throw new ArgumentNullException(nameof(service));
+        }
+
+        public ValidationResults Register(RegistrationDTO request)
+        {
+
+            ValidationResults result = _validation.Validate(request);
+
+            if (!result.IsValid)
+                return result;
+            
+
+            _service.RegisterUser(request);
+
+            return result;
+
+        }
+
+    }
+}
---
+++ Coffee.Kiosk.CMS/Forms/AccountsTab/RegisterEmp/RegisterControl.cs
@@ -0,0 +1,78 @@
+﻿using Coffee.Kiosk.CMS.Controllers;
+using Coffee.Kiosk.CMS.DTOs;
+using Mysqlx.Session;
+using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coffee.Kiosk.CMS
+{
+    public partial class RegisterControl : UserControl
+    {
+
+
+        public AdminControlForm? ParentFormReference { get; set; }
+
+        //From here.
+        private readonly RegistrationController _controller;
+
+        public RegisterControl(RegistrationController registrationController)
+        {
+            InitializeComponent();
+            _controller = registrationController ?? throw new ArgumentNullException(nameof(registrationController));
+        }
+        //To here, This is the basic structure of initializing your Dependency injections on each class.
+        //Dependency Injections are just another way of calling a class in another class. Head hurts.
+
+        private void RegisterControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+
+            InputCollection();
+
+        }
+
+        private void InputCollection()
+        {
+            var newRequest = new RegistrationDTO
+            {
+
+                FullName = fullNameTextBox.Text,
+                PhoneNumber = phoneNumTextBox.Text,
+                Email = emailAddressTextBox.Text,
+                EmergencyNumber = emergencyContactTextBox.Text,
+                JobTitle = jobTitleTextBox.Text,
+                Salary = salaryTextBox.Text
+            };
+
+            var result = _controller.Register(newRequest);
+
+            if (!result.IsValid)
+            {
+                MessageBox.Show(string.Join(Environment.NewLine, result.Errors.Values), "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (result.IsValid)
+            {
+                MessageBox.Show("Registration successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+        }
+
+
+    }
+}
