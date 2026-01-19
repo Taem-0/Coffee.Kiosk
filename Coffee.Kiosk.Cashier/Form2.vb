9a6b2b756f12dce26e351bbb1762d1c464d27015
ljclouds01 <louiejein.banting012906@gmail.com>
2026-01-19 10:08:27 +0800

    basta HAHAHAHAH

--- Coffee.Kiosk.Cashier/Form2.vb
+++ Coffee.Kiosk.Cashier/Form2.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form2
+    Private Sub btnLogIn2_Click(sender As Object, e As EventArgs) Handles btnLogIn2.Click
+        Dim frm As New Form4
 
+        frm.Show()
+        Me.Hide()
+    End Sub
 End Class
