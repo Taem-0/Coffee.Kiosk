0b3fdb79f78b722f82c6404f0fe936391cb0f270
lovenads <lalata.bernadettebscs2024@gmail.com>
2026-02-01 21:52:46 +0800

    pls form1 and form2 be good to meh

--- Coffee.Kiosk.OrderStatusDisplaySystem/Form1.Designer.vb
+++ Coffee.Kiosk.OrderStatusDisplaySystem/Form1.Designer.vb
@@ -22,10 +22,149 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        ListView1 = New ListView()
+        ListView2 = New ListView()
+        ListView3 = New ListView()
+        ListView4 = New ListView()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' ListView1
+        ' 
+        ListView1.BackColor = Color.OldLace
+        ListView1.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ListView1.ForeColor = Color.FromArgb(CByte(128), CByte(64), CByte(0))
+        ListView1.Location = New Point(576, 64)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(320, 907)
+        ListView1.TabIndex = 0
+        ListView1.UseCompatibleStateImageBehavior = False
+        ' 
+        ' ListView2
+        ' 
+        ListView2.BackColor = Color.OldLace
+        ListView2.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ListView2.ForeColor = Color.FromArgb(CByte(128), CByte(64), CByte(0))
+        ListView2.Location = New Point(907, 64)
+        ListView2.Name = "ListView2"
+        ListView2.Size = New Size(320, 905)
+        ListView2.TabIndex = 1
+        ListView2.UseCompatibleStateImageBehavior = False
+        ' 
+        ' ListView3
+        ' 
+        ListView3.BackColor = Color.OldLace
+        ListView3.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ListView3.ForeColor = Color.FromArgb(CByte(128), CByte(64), CByte(0))
+        ListView3.Location = New Point(1238, 64)
+        ListView3.Name = "ListView3"
+        ListView3.Size = New Size(320, 905)
+        ListView3.TabIndex = 2
+        ListView3.UseCompatibleStateImageBehavior = False
+        ' 
+        ' ListView4
+        ' 
+        ListView4.BackColor = Color.OldLace
+        ListView4.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ListView4.ForeColor = Color.FromArgb(CByte(128), CByte(64), CByte(0))
+        ListView4.Location = New Point(1568, 64)
+        ListView4.Name = "ListView4"
+        ListView4.Size = New Size(320, 905)
+        ListView4.TabIndex = 3
+        ListView4.UseCompatibleStateImageBehavior = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(616, 25)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(236, 33)
+        Label1.TabIndex = 4
+        Label1.Text = "Gcash Payment"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(962, 25)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(217, 33)
+        Label2.TabIndex = 5
+        Label2.Text = "Cash Payment"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.White
+        Label3.Location = New Point(1270, 26)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(270, 33)
+        Label3.TabIndex = 6
+        Label3.Text = "Preparing Orders "
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Arial Rounded MT Bold", 21.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.White
+        Label4.Location = New Point(1595, 26)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(270, 33)
+        Label4.TabIndex = 7
+        Label4.Text = "Ready for Pick-Up"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 948)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 41)
+        Button1.TabIndex = 8
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.orderingdisplay_bg
+        ClientSize = New Size(1904, 1001)
+        ControlBox = False
+        Controls.Add(Button1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ListView4)
+        Controls.Add(ListView3)
+        Controls.Add(ListView2)
+        Controls.Add(ListView1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ListView2 As ListView
+    Friend WithEvents ListView3 As ListView
+    Friend WithEvents ListView4 As ListView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
 
 End Class
---
+++ Coffee.Kiosk.OrderStatusDisplaySystem/Monitoring buttons.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Monitoring_buttons
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        btnPreparing = New Button()
+        btnReady = New Button()
+        btnComplete = New Button()
+        SuspendLayout()
+        ' 
+        ' btnPreparing
+        ' 
+        btnPreparing.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(128))
+        btnPreparing.Font = New Font("Arial Rounded MT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnPreparing.ForeColor = Color.White
+        btnPreparing.Location = New Point(35, 8)
+        btnPreparing.Name = "btnPreparing"
+        btnPreparing.Size = New Size(386, 58)
+        btnPreparing.TabIndex = 0
+        btnPreparing.Text = "Preparing Orders"
+        btnPreparing.UseVisualStyleBackColor = False
+        ' 
+        ' btnReady
+        ' 
+        btnReady.BackColor = Color.LightGreen
+        btnReady.Font = New Font("Arial Rounded MT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnReady.ForeColor = Color.White
+        btnReady.Location = New Point(459, 9)
+        btnReady.Name = "btnReady"
+        btnReady.Size = New Size(386, 58)
+        btnReady.TabIndex = 1
+        btnReady.Text = "Ready for Pick-Up"
+        btnReady.UseVisualStyleBackColor = False
+        ' 
+        ' btnComplete
+        ' 
+        btnComplete.BackColor = SystemColors.ActiveCaption
+        btnComplete.Font = New Font("Arial Rounded MT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        btnComplete.ForeColor = Color.White
+        btnComplete.Location = New Point(878, 9)
+        btnComplete.Name = "btnComplete"
+        btnComplete.Size = New Size(386, 58)
+        btnComplete.TabIndex = 2
+        btnComplete.Text = "Order's Complete"
+        btnComplete.UseVisualStyleBackColor = False
+        ' 
+        ' Monitoring_buttons
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(btnComplete)
+        Controls.Add(btnReady)
+        Controls.Add(btnPreparing)
+        Name = "Monitoring_buttons"
+        Size = New Size(1299, 77)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnPreparing As Button
+    Friend WithEvents btnReady As Button
+    Friend WithEvents btnComplete As Button
+
+End Class
