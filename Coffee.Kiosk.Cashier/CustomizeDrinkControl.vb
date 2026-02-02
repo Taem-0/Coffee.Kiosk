4efc5fc597055888e3d93d1832007dab16ccb1be
ljclouds01 <louiejein.banting012906@gmail.com>
2026-02-02 10:31:57 +0800

    i dont know what happened

---
+++ Coffee.Kiosk.Cashier/CustomizeDrinkControl.vb
@@ -0,0 +1,99 @@
+﻿Public Class CustomizeDrinkControl
+
+    Private _drink As DrinkItem
+    Private _currentPrice As Decimal
+
+    Public Event OrderAdded(order As OrderItem)
+
+    Public Sub LoadDrink(drink As DrinkItem)
+        _drink = drink
+        _currentPrice = drink.BasePrice
+
+        lblDrinkName.Text = drink.Name
+        lblBasePrice.Text = "₱" & _currentPrice.ToString("0.00")
+
+        grpTemp.Enabled = (drink.Category = DrinkCategory.Coffee)
+
+        rbIce.Checked = True
+        rbHot.Checked = False
+
+        grpIceLevel.Enabled = True
+        CalculateTotal()
+    End Sub
+
+    Private Sub CalculateTotal()
+        If _drink Is Nothing Then Exit Sub
+
+        Dim sizePrice As Decimal = 0
+        If rbMedium.Checked Then sizePrice = 10D
+        If rbLarge.Checked Then sizePrice = 20D
+
+        Dim toppingsPrice As Decimal = 0
+        If rbVelvet.Checked Then toppingsPrice += 25
+        If rbCoffeeJelly.Checked Then toppingsPrice += 25
+        If rbJellyStrips.Checked Then toppingsPrice += 25
+        If rbButterCreme.Checked Then toppingsPrice += 25
+        If rbSeasaltCreme.Checked Then toppingsPrice += 25
+
+        _currentPrice = _drink.BasePrice + sizePrice + toppingsPrice
+        lblTotal.Text = "₱" & (_currentPrice * nudQty.Value).ToString("0.00")
+    End Sub
+
+    Private Sub btnAddOrder_Click(sender As Object, e As EventArgs) Handles btnAddOrder.Click
+        Dim order As New OrderItem With {
+            .Drink = _drink,
+            .Temperature = If(rbHot.Checked, "Hot", "Iced"),
+            .Size = If(rbSmall.Checked, "Small", If(rbMedium.Checked, "Medium", "Large")),
+            .Toppings = GetSelectedToppings(),
+            .Quantity = nudQty.Value,
+            .TotalPrice = _currentPrice * nudQty.Value
+        }
+
+        RaiseEvent OrderAdded(order)
+        CloseControl()
+    End Sub
+
+    Private Sub OptionChanged(sender As Object, e As EventArgs) _
+        Handles rbSmall.CheckedChanged, rbMedium.CheckedChanged, rbLarge.CheckedChanged,
+                rbHot.CheckedChanged, rbIce.CheckedChanged,
+                rbVelvet.CheckedChanged, rbCoffeeJelly.CheckedChanged,
+                rbJellyStrips.CheckedChanged, rbButterCreme.CheckedChanged,
+                rbSeasaltCreme.CheckedChanged, nudQty.ValueChanged
+
+        If _drink Is Nothing Then Exit Sub
+
+        If _drink.Category = DrinkCategory.Coffee Then
+            grpIceLevel.Enabled = rbIce.Checked
+        Else
+            rbHot.Checked = False
+            rbIce.Checked = True
+            grpIceLevel.Enabled = True
+        End If
+
+        CalculateTotal()
+    End Sub
+
+    Private Function GetSelectedToppings() As List(Of String)
+        Dim toppings As New List(Of String)
+        If rbVelvet.Checked Then toppings.Add("Velvet")
+        If rbCoffeeJelly.Checked Then toppings.Add("Coffee Jelly")
+        If rbJellyStrips.Checked Then toppings.Add("Jelly Strips")
+        If rbButterCreme.Checked Then toppings.Add("Butter Creme")
+        If rbSeasaltCreme.Checked Then toppings.Add("Seasalt Creme")
+        Return toppings
+    End Function
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        CloseControl()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        CloseControl()
+    End Sub
+
+    Private Sub CloseControl()
+        Me.Parent.Controls.Remove(Me)
+        Me.Dispose()
+    End Sub
+
+End Class
---
+++ Coffee.Kiosk.Cashier/OrderCustomize.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class OrderCustomize
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        CustomizePanel = New Panel()
+        SuspendLayout()
+        ' 
+        ' CustomizePanel
+        ' 
+        CustomizePanel.Dock = DockStyle.Fill
+        CustomizePanel.Location = New Point(0, 0)
+        CustomizePanel.Name = "CustomizePanel"
+        CustomizePanel.Size = New Size(974, 689)
+        CustomizePanel.TabIndex = 1
+        ' 
+        ' OrderCustomize
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.OldLace
+        ClientSize = New Size(974, 689)
+        ControlBox = False
+        Controls.Add(CustomizePanel)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Name = "OrderCustomize"
+        StartPosition = FormStartPosition.CenterScreen
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents CustomizePanel As Panel
+End Class
