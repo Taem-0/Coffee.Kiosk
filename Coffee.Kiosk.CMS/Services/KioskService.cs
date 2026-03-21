6cb7f64dded2b481683bfa7b7edd2bbd63ee4e50
Mult0 <taezarex@gmail.com>
2026-03-21 11:40:40 +0800

    FullyFunctionalKioskBannerS
    CHANGES:
        -Upload, Clear and edit banners for kiosk
    
    NOTES:
        -Later develop the kiosk preview above the banner upload

--- Coffee.Kiosk.CMS/Services/KioskService.cs
+++ Coffee.Kiosk.CMS/Services/KioskService.cs
@@ -9,8 +9,13 @@ namespace Coffee.Kiosk.CMS.Services
         public KioskService(CoffeeKDB.KioskDBManager dbManager) => _dbManager = dbManager;
 
         public List<KioskBanner> GetAllBanners() => _dbManager.GetAllBanners();
+
         public void AddBanner(KioskBanner banner) => _dbManager.AddBanner(banner);
+
         public void UpdateBannerFilePath(int id, string path) => _dbManager.UpdateBannerFilePath(id, path);
+
+        public void UpdateBanner(KioskBanner banner) => _dbManager.UpdateBanner(banner);
+
         public void DeleteBanner(int id) => _dbManager.DeleteBanner(id);
     }
 }
